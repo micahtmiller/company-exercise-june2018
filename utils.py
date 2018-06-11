25ec3189672d3e46320d4cb8fdeea535ea22f30f
Micah Miller <MMiller18@slb.com>
2018-06-11 10:49:59 -0600

    adding player data function for better looking results

--- utils.py
+++ utils.py
@@ -18,6 +18,11 @@ def import_data(kind):
         logging.info('No file kind found')
     return df
 
+def add_player_details(df):
+    player_df = import_data('player')
+    batting_player_df = df.merge(player_df, how='inner', on='playerID')
+    return batting_player_df
+
 def group_by_player_year_league(df):
     grouped_df = df.groupby(by=['playerID', 'yearID', 'league'])['G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'CS'].sum().reset_index()
     return grouped_df
