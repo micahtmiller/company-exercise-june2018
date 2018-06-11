90b926cc3259e84f823f8b255833a17bb02ac01a
Micah Miller <MMiller18@slb.com>
2018-06-11 14:30:00 -0600

    make output a little prettier

--- python_code/sportsball.py
+++ python_code/sportsball.py
@@ -7,8 +7,11 @@ import config
 
 def main():
     batting_df = utils.import_data('batting')
+    print('\n***Getting Most Improved Batting Average***')
     batting_avg.get_most_improved(batting_df, config.MOST_IMPROVED_START_YEAR, config.MOST_IMPROVED_END_YEAR, config.BATTING_AVG_MIN_AT_BATS)
+    print('\n***Getting Slugging Percentage***')
     slugging_percentage.get_slugging_percentage(batting_df, config.SLUGGING_TEAM, config.SLUGGING_YEAR)
+    print('\n***Getting Triple Crown Winners***')
     triple_crown_years = range(config.TRIPLE_CROWN_START_YEAR, config.TRIPLE_CROWN_END_YEAR + 1)
     leagues = config.TRIPLE_CROWN_LEAGUES
     for year in triple_crown_years:
