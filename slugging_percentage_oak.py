6bad9287ebdcda01b295f1ce98b1e76d1456e2c8
Micah Miller <MMiller18@slb.com>
2018-06-10 19:54:45 -0600

    added slugging percentage calculations

---
+++ slugging_percentage_oak.py
@@ -0,0 +1,28 @@
+import os
+
+import pandas
+
+import config
+import utils
+
+def filter_oakland_2007(df):
+    filtered_df = df[(df['teamID'] == 'OAK') & (df['yearID'] == 2007)].copy()
+    return filtered_df
+
+def calculate_sluggers(df):
+    df['slugging_perc'] = (
+        (
+            (df['H'] - df['2B'] - df['3B'] - df['HR']) + (2 * df['2B']) + (3 * df['3B']) + (4 * df['HR'])
+        ) / df['AB']
+    )
+    return df[['playerID', 'slugging_perc']].sort_values(by=['slugging_perc'], ascending=False).reset_index()
+
+def main():
+    batting_df, player_df = utils.import_data()
+    filtered_df = filter_oakland_2007(batting_df)
+    slugging_perc_df = calculate_sluggers(filtered_df)
+    print('All Players Slugging Percentage')
+    print(slugging_perc_df)
+
+if __name__ == '__main__':
+    main()
---
+++ test_slugging_percentage_oak.py
@@ -0,0 +1,61 @@
+import pytest
+import pandas
+
+import slugging_percentage_oak
+
+import config
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+def test_filter_oakland_2007():
+    data = [
+        {'teamID': 'OAK', 'yearID': 2007},
+        # {'teamID': 'OAK', 'yearID': 2008},
+        # {'teamID': 'OTH', 'yearID': 2007},
+    ]
+    test_df = pandas.DataFrame(data)
+    filtered_df = slugging_percentage_oak.filter_oakland_2007(test_df)
+    assert len(filtered_df) == 1
+    data = [
+        # {'teamID': 'OAK', 'yearID': 2007},
+        {'teamID': 'OAK', 'yearID': 2008},
+        # {'teamID': 'OTH', 'yearID': 2007},
+    ]
+    test_df = pandas.DataFrame(data)
+    filtered_df = slugging_percentage_oak.filter_oakland_2007(test_df)
+    assert len(filtered_df) == 0
+    data = [
+        # {'teamID': 'OAK', 'yearID': 2007},
+        # {'teamID': 'OAK', 'yearID': 2008},
+        {'teamID': 'OTH', 'yearID': 2007},
+    ]
+    test_df = pandas.DataFrame(data)
+    filtered_df = slugging_percentage_oak.filter_oakland_2007(test_df)
+    assert len(filtered_df) == 0
+    
+def test_calculate_sluggers():
+    data = [
+        {'playerID': 'test', 'H': 4, '2B': 1, '3B': 1, 'HR': 1, 'AB': 5}
+    ]
+    # (4 -1(double) -1(triple) -1(hr)) = 1
+    # (1 + 2 * 1 + 3 * 1 + 4 * 1)  = 10
+    # 10 / 5 = 2.0
+    test_df = pandas.DataFrame(data)
+    sluggers_df = slugging_percentage_oak.calculate_sluggers(test_df)
+    logging.debug(sluggers_df)
+    assert sluggers_df['slugging_perc'].values[0] == 2.0 
+    data = [
+        {'playerID': 'test', 'H': 8, '2B': 2, '3B': 2, 'HR': 2, 'AB': 40}
+    ]
+    # (8 -2(double) -2(triple) -2(hr)) = 2
+    # (2 + 2 * 2 + 3 * 2 + 4 * 2)  = 20
+    # 20 / 40 = .5
+    test_df = pandas.DataFrame(data)
+    sluggers_df = slugging_percentage_oak.calculate_sluggers(test_df)
+    logging.debug(sluggers_df)
+    assert sluggers_df['slugging_perc'].values[0] == 0.5
+
+if __name__ == '__main__':
+    test_calculate_sluggers()
