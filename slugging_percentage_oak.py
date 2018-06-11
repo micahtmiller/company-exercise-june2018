c45e38099601b02dfc234ffbf1e8d8b793062e38
Micah Miller <MMiller18@slb.com>
2018-06-11 08:38:40 -0600

    modified import data function * input of kind (batting, player, pitching) * return the corresponding dataframe

--- slugging_percentage_oak.py
+++ slugging_percentage_oak.py
@@ -1,9 +1,11 @@
-import os
+import logging
 
 import pandas
 
 import config
 import utils
+
+logging.basicConfig(level=logging.DEBUG)
 
 def filter_oakland_2007(df):
     filtered_df = df[(df['teamID'] == 'OAK') & (df['yearID'] == 2007)].copy()
@@ -18,7 +20,7 @@ def calculate_sluggers(df):
     return df[['playerID', 'slugging_perc']].sort_values(by=['slugging_perc'], ascending=False).reset_index()
 
 def main():
-    batting_df, player_df = utils.import_data()
+    batting_df = utils.import_data('batting')
     filtered_df = filter_oakland_2007(batting_df)
     slugging_perc_df = calculate_sluggers(filtered_df)
     print('All Players Slugging Percentage')
--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
+import logging
 import os
 
 import pandas
 
 import config
 
-def import_data():
-    batting_df = pandas.read_csv(os.path.join(config.DATA_FILES_LOCATION, 'Batting-07-12.csv'))
-    player_df = pandas.read_csv(os.path.join(config.DATA_FILES_LOCATION, 'Master-small.csv'))
-    # pitching_df = pandas.read_csv(os.path.join(file_location, 'xxxx.csv'))
-    return batting_df, player_df
+logging.basicConfig(level=logging.DEBUG)
+
+def import_data(kind):
+    if kind == 'batting':
+        df = pandas.read_csv(os.path.join(config.DATA_FILES_LOCATION, 'Batting-07-12.csv'))
+    elif kind == 'player':
+        df = pandas.read_csv(os.path.join(config.DATA_FILES_LOCATION, 'Master-small.csv'))
+    elif kind == 'pitching':
+        df = pandas.read_csv(os.path.join(config.DATA_FILES_LOCATION, 'xxxx.csv'))
+    else:
+        logging.info('No file kind found')
+    return df
+
+def group_by_player_year_league(df):
+    grouped_df = df.groupby(by=['playerID', 'yearID', 'league'])['G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'CS'].sum().reset_index()
+    return grouped_df
